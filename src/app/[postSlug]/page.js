dfbe97949453d6036861bca4208237fa62143d9b
eugenefischer <66030419+eugenefischer@users.noreply.github.com>
2024-05-21 13:31:25 -0500

    Exercise 2

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,23 +1,17 @@
-import React from 'react';
+import React from "react";
+import { loadBlogPost } from "@/helpers/file-helpers";
+import BlogHero from "@/components/BlogHero";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import styles from "./postSlug.module.css";
 
-import BlogHero from '@/components/BlogHero';
-
-import styles from './postSlug.module.css';
-
-function BlogPost() {
+async function BlogPost({ params }) {
+  const { content } = await loadBlogPost(`${params.postSlug}`);
+  // console.log(content);
   return (
     <article className={styles.wrapper}>
-      <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
-      />
+      <BlogHero title="Example post!" publishedOn={new Date()} />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import styles from "./homepage.module.css";
 
 async function Home() {
   const blogPosts = await getBlogPostList();
-  // console.log(blogPosts);
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
