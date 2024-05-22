376bcc589092fa3fff06a72023a1f929cf875ab7
eugenefischer <66030419+eugenefischer@users.noreply.github.com>
2024-05-22 11:01:59 -0500

    Exercises 3 & 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,6 +3,16 @@ import { loadBlogPost } from "@/helpers/file-helpers";
 import BlogHero from "@/components/BlogHero";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
+import { BLOG_TITLE } from "@/constants";
+import CodeSnippet from "@/components/CodeSnippet";
+
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(`${params.postSlug}`);
+  return {
+    title: `${frontmatter.title} - ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const post = await loadBlogPost(`${params.postSlug}`);
@@ -14,7 +24,7 @@ async function BlogPost({ params }) {
         publishedOn={post.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={post.content} />
+        <MDXRemote source={post.content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { getBlogPostList } from "@/helpers/file-helpers";
 import BlogSummaryCard from "@/components/BlogSummaryCard";
-
 import styles from "./homepage.module.css";
 
 async function Home() {
